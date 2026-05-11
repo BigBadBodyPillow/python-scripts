536e62bcb9b42e15193494a0bb02546654ae6af6
.sadness <119925640+BigBadBodyPillow@users.noreply.github.com>
2026-05-12 01:52:05 +0200

    Add files via upload

--- Path of Exile/decoration_offset.py
+++ Path of Exile/decoration_offset.py
@@ -1,18 +1,22 @@
 import re
 import sys
 
+x = 100
+y = 100
+
+
 def adjust_coordinates_raw(input_file, output_file):
     with open(input_file, "r", encoding="utf-8-sig") as f:
         text = f.read()
 
     # Add 100 to all x values
     text = re.sub(r'"x":\s*(\d+)', 
-                  lambda m: f'"x": {int(m.group(1)) + 100}', 
+                  lambda m: f'"x": {int(m.group(1)) + x}', 
                   text)
 
     # Add 100 to all y values
     text = re.sub(r'"y":\s*(\d+)', 
-                  lambda m: f'"y": {int(m.group(1)) + 100}', 
+                  lambda m: f'"y": {int(m.group(1)) + y}', 
                   text)
 
     with open(output_file, "w", encoding="utf-8") as f:
